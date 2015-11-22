e5c428edb2eb16b3b7d70cb25cb7a6236ea80a5c
Stijn Opheide <stijn.opheide@gmail.com>
2015-11-22 11:02:21 +0100

    add support for kubernetes secrets

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# check if we have a secret to include in the config
+
+if [ -f /etc/secrets/thumbor/security-key ]; then
+	export SECURITY_KEY=\'`cat /etc/secrets/thumbor/security-key`\'
+fi
+
 # Configure thumbor from environment
 printenv | grep -f /opt/thumbor-options.conf > /opt/thumbor.conf
 
